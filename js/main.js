5f239aa04cf62e0b3727f8a0c819bf205deca5e7
PechPavlo <61708124+PechPavlo@users.noreply.github.com>
2020-06-07 20:22:38 +0300

    add section "our-trip"

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ $('.fairy-tail__slider').slick({
     fade: true
 });
 });
+
+$(function(){
+$('.our-trip__slider').slick({
+    prevArrow:'<button type="button" class="slick-btn slick-prev"><img src="./images/arrow-prev.svg" alt=""></button>',
+    nextArrow:'<button type="button" class="slick-btn slick-next"><img src="./images/arrow-next.svg" alt=""></button>',
+    autoplay: true,
+    fade: true
+});
+});
